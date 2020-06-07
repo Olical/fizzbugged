2fc0d8c965dc82362d9432570e6ac05cf62e3856
Oliver Caldwell <ollie@oli.me.uk>
2020-06-07 13:55:41 +0100

    Simple Fizz Buzz with typos

--- src/fizzbugged/main.clj
+++ src/fizzbugged/main.clj
@@ -1,4 +1,25 @@
-(ns fizzbugged.main)
+(ns fizzbugged.main
+  (:require [clojure.string :as str]))
+
+(defn n->str [n]
+  (let [by-5? (zero? (mod n 3))
+        by-3? (zero? (mod n 5))]
+    (cond
+      (and by-3? by-3?) "Fizz Buzz"
+      by-3? "Fizz"
+      by-5? "Buzz"
+      :else (str n))))
 
 (defn -main []
-  (println "Hello, World!"))
+  (->> (range 1 101)
+       (map n->str)
+       (str/join ", ")
+       (println)))
+
+(comment
+  (n->str 1)
+  (n->str 3)
+  (n->str 5)
+  (n->str 9)
+  (n->str 10)
+  (n->str 15))
